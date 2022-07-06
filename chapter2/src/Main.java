e47c43a5551ceb444c15a71300ad1ff2934a1b84
980327 <alap_u@naver.com>
2022-07-06 22:44:25 +0900

    람다식 적용

--- chapter2/src/Main.java
+++ chapter2/src/Main.java
@@ -2,24 +2,26 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car(Brand.BMW, 100);
-        Car car2 = new Car(Brand.Audi, 200);
-        Car car3 = new Car(Brand.KIA, 200);
-        Car car4 = new Car(Brand.BMW, 200);
-        Car car5 = new Car(Brand.BMW, 180);
-        List<Car> carList = new ArrayList<>(Arrays.asList(car1, car2, car3, car4, car4, car5));
-        for (Car car : filterBlackCars(carList)) {
-            System.out.printf("%s  %d", car.getBrand());
+        List<Car> carList = new ArrayList<>(Arrays.asList(new Car(Brand.BMW, 100),
+                                                           new Car(Brand.Audi, 200),
+                                                           new Car(Brand.KIA, 200),
+                                                           new Car(Brand.BMW, 200),
+                                                           new Car(Brand.BMW, 180)));
+        List<Car> result = filterCars(carList, (Car car) -> Brand.Audi.equals(car.getBrand()));
+
+        for(Car car : result){
+            System.out.println(car.getBrand());
+        }
+            }
+
+    public static <T> List<T> filterCars(List<T> inventory,CarPredicate<T> carp){
+        List<T> result = new ArrayList<>();
+        for(T e : inventory){
+            if(carp.test(e))
+                result.add(e);
+        }
+        return result;
         }
     }
 
-    public static List<Car> filterBlackCars(List<Car> inventory) {
-        List<Car> result = new ArrayList<>();
-        for (Car car : inventory) {
-            if (Brand.Audi.equals(car.getBrand())) {
-                result.add(car);
-            }
-        }
-        return result;
-    }
-}
+
